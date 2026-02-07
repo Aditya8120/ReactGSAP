435e65a2d05323aca28aedddb936f4ea02e84f9d
Aditya8120 <aditya3.230101009@iiitbh.ac.in>
2026-02-08 00:00:58 +0530

    NavBar positioning

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/agence' element={<Agence />} />
--- src/components/Navbar/Menu.jsx
+++ src/components/Navbar/Menu.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 const Menu = () => {
   return (
-    <div>
+    <div className='h-full w-full bg-black'>
       <div>
         <h1>Projects</h1>
-        <div></div>
+        <div>
+          <h2>Lorem, ipsum dolor.</h2>
+          <img src="https://i.pinimg.com/736x/47/2a/dd/472add28786aa328599861a37a1371ab.jpg" alt="img" />
+          <h2>Lorem, ipsum dolor.</h2>
+          <img src="https://i.pinimg.com/736x/47/2a/dd/472add28786aa328599861a37a1371ab.jpg" alt="img" />
+        </div>
       </div>
     </div>
   )
